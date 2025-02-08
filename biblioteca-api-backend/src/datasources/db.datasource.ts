745aac24a4af82993894025343e25643982af3b1
a045580 <a045580@umaia.pt>
2025-02-08 05:23:45 +0000

    Detalhes finais

--- biblioteca-api-backend/src/datasources/db.datasource.ts
+++ biblioteca-api-backend/src/datasources/db.datasource.ts
@@ -3,26 +3,25 @@ import {juggler} from '@loopback/repository';
 
 const config = {
   name: 'db',
-  connector: 'mysql',
+  connector: 'mysql2',
   host: process.env.DB_HOST || 'biblioteca-database',
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '1f18ac3cd637',
   database: process.env.DB_DATABASE || 'biblioteca_m3_recurso',
-  authPlugin: 'mysql_native_password',
   charset: 'utf8mb4'
 };
 
 @lifeCycleObserver('datasource')
 export class DbDataSource extends juggler.DataSource
- implements LifeCycleObserver {
- static dataSourceName = 'db';
- static readonly defaultConfig = config;
+  implements LifeCycleObserver {
+  static dataSourceName = 'db';
+  static readonly defaultConfig = config;
 
- constructor(
-   @inject('datasources.config.db', {optional: true})
-   dsConfig: object = config,
- ) {
-   super(dsConfig);
- }
+  constructor(
+    @inject('datasources.config.db', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
 }
